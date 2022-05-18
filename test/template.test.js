7ac92691bc43eda78ffe3bcad6a0e2f590d3003b
JobNs <job.vandertas@ns.nl>
2022-05-18 10:33:34 +0200

    8 and 10 places invalid

--- test/template.test.js
+++ test/template.test.js
@@ -1,12 +1,21 @@
 const createNewPlayingGrid = require('../src/template');
 
+const newPlayingGrid = createNewPlayingGrid()
 
 describe('This test suite test a Tic Tac Toe game', () => {
     describe('We have a valid playing grid for the game, so:', () => {
         it('- A grid of 9 places is valid', () => {
-            newPlayingGrid = createNewPlayingGrid()
+            
             lengthNewPlayingGrid = newPlayingGrid.length
             expect(lengthNewPlayingGrid == 9).toEqual(true);
+        });
+        it('- A grid of 8 places is invalid', () => {
+            lengthNewPlayingGrid = newPlayingGrid.length
+            expect(lengthNewPlayingGrid == 8).toEqual(false);
+        });
+        it('- A grid of 10 places is invalid', () => {
+            lengthNewPlayingGrid = newPlayingGrid.length
+            expect(lengthNewPlayingGrid == 10).toEqual(false);
         });
     });
 });
